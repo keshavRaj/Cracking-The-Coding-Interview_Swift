56fa8836cf23bdaff53d2d31f9434884a23f5c47
Keshav Raj <keshavraj@Keshavs-MacBook-Pro.local>
2024-01-03 21:28:25 +0530

    Initial Commit with Xcode Project created

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/CrackingTheCodingInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrackingTheCodingInterviewApp.swift
+//  CrackingTheCodingInterview
+//
+//  Created by Keshav Raj on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CrackingTheCodingInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
